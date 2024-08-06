6f37be32764de269fd92431ec7f5dbcebe0f69f8
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-08-05 21:06:51 -0300

    criado update linhas

---
+++ js/favorites.js
@@ -0,0 +1,28 @@
+class favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+  }
+
+}
+
+export class favoritesView extends favorites {
+  constructor(root) {
+    super(root)
+
+    this.update()
+  }
+
+  update() {
+    this.removeAllTr()
+  }
+
+  removeAllTr() {
+    const tbody = this.root.querySelector('table tbody')
+  
+    tbody.querySelectorAll('tr').forEach((tr) => {
+      tr.remove()
+    })
+    
+  }
+}
+
