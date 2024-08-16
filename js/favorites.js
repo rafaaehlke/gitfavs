04a0b9a3e3a1e8aa992aa4d8f06a3d0a5bb8ca49
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-08-16 20:41:44 -0300

    finalizado

--- js/favorites.js
+++ js/favorites.js
@@ -1,26 +1,53 @@
+import { GithubUser } from "./githubUser.js"
+
 export class favorites {
   constructor(root) {
-    
+
     this.root = document.querySelector(root)
     this.load()
+    
   }
 
-  load() { 
-   this.entries = [
-    {
-      login: 'rafaaehlke',
-      name: 'Rafael Ehlke',
-      public_repos: '10',
-      followers: '5'
-  },
-  {
-    login: 'fronzinha',
-    name: 'João Fronza',
-    public_repos: '5',
-    followers: '3'
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    
   }
-]
 
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+ async add(username) {
+    try {
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if(userExists) {
+        throw new Error('Usuário já cadastrado!')
+      }
+
+      const user = await GithubUser.search(username)
+
+      if(user.login === undefined) {
+        throw new Error('Usuário não encontrado')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch(error)  {
+      alert(error.message)
+    }
+ }
+
+  delete(user) {
+    // Higher-order functions (map, filter, find, reduce)
+    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+    this.entries = filteredEntries
+    this.update()
+    this.save()
   }
 
 }
@@ -32,29 +59,48 @@ export class favoritesView extends favorites {
     this.tbody = this.root.querySelector('table tbody')
 
     this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+      
+        this.add(value)
+    }
+
   }
 
   update() {
     this.removeAllTr()
 
-    this.entries.forEach( user => {
+    this.entries.forEach(user => {
       const row = this.createRow()
+
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Imagem de ${user.name}`
-      row.querySelector('.user p' ).textContent = user.name
+      row.querySelector('.user p').textContent = user.name
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repository').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
-      
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Gostaria de remover este usuário?')
+       
+        if (isOk) {
+          this.delete(user)
+        }
+      }
       
       this.tbody.append(row)
-  })
+    })
 
   }
 
   createRow() {
     // elemento criado pela dom
-    const tr = document.createElement('tr')
+    const tr = document.createElement("tr")
 
     // conteudo do tr
     tr.innerHTML = `
@@ -97,7 +143,7 @@ export class favoritesView extends favorites {
     this.tbody.querySelectorAll('tr').forEach((tr) => {
       tr.remove()
     })
-    
+
   }
 }
 
